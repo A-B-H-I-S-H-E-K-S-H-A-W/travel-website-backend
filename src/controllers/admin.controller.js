962b04166518b5d6fe520644cc99d5fa65f9f816
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-04-28 21:17:12 +0530

    Made admin login and registration functional routes for registration and login

---
+++ src/controllers/admin.controller.js
@@ -0,0 +1,78 @@
+import { Admin } from "../models/admin.models.js";
+import Decrypt from "../utils/Decrypt.js";
+import Encrypt from "../utils/Encrypt.js";
+import jwt from "jsonwebtoken";
+
+export async function registeradmin(req, res) {
+  try {
+    const { username, email, password, domain } = req.body;
+
+    const existingAdmin = await Admin.findOne({ email });
+
+    if (existingAdmin) {
+      return res
+        .status(400)
+        .json({ message: "Admin already exists, Please login !" });
+    }
+
+    const hashedPassword = await Encrypt(password);
+
+    const newAdmin = new Admin({
+      username,
+      email,
+      domain,
+      password: hashedPassword,
+    });
+
+    await newAdmin.save();
+    res.status(201).json({ message: "Admin successfully registered" });
+  } catch (error) {
+    console.log("Admin registration error ::::", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function loginadmin(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    const admin = await Admin.findOne({ email });
+
+    if (!admin) {
+      return res.status(400).json({ message: "User dosen't exists" });
+    }
+
+    await Decrypt(password, admin.password)
+      .then((isMatch) => {
+        if (isMatch) {
+          const token = jwt.sign(
+            { id: admin._id },
+            process.env.ADMIN_JWT_SECRET,
+            {
+              expiresIn: process.env.ADMIN_JWT_EXPIRE,
+            }
+          );
+
+          res.status(200).json({
+            message: "Login Successfully",
+            token,
+            admin: {
+              id: admin._id,
+              username: admin.username,
+              email: admin.email,
+              domain: admin.domain,
+            },
+          });
+        } else {
+          res.status(400).json({ message: "Login Failed, Password Incorrect" });
+        }
+      })
+      .catch((error) => {
+        console.log("Error during password decryption ::::", error);
+        res.status(500).json({ message: "Server Error" });
+      });
+  } catch (error) {
+    console.log("ERROR CREATING USER :::", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import { loginadmin, registeradmin } from "../controllers/admin.controller.js";
 
 export const adminRouter = Router();
 
 adminRouter.get("/", (req, res) => {
   res.send("Hello");
 });
+
+adminRouter.post("/register", registeradmin);
+adminRouter.post("/auth", loginadmin);
