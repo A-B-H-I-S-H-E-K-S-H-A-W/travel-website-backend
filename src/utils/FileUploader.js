f9cf1504ad68679c796b0d2b1eb5aee0f3cea6e0
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-04-29 10:19:34 +0530

    Renamed Image uploader to file uploader

---
+++ src/utils/FileUploader.js
@@ -0,0 +1,16 @@
+async function FileUploader(image) {
+  try {
+    const uniqueName =
+      Date.now() + Math.floor(Math.random() * 1000000) + "-" + image.name;
+    const uploadPath = `./src/public/images/${uniqueName}`;
+
+    await image.mv(uploadPath);
+
+    return uniqueName;
+  } catch (error) {
+    console.log("Error while uploading the image :::", error);
+    throw error;
+  }
+}
+
+export default FileUploader;
