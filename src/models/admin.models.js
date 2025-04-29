40f50774809249d5b02fdb1733c94f526e6495cf
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-04-29 19:55:43 +0530

    Added timestamps

--- src/models/admin.models.js
+++ src/models/admin.models.js
@@ -1,63 +1,68 @@
 import { Schema, model } from "mongoose";
 
-const adminSchema = Schema({
-  username: {
-    type: String,
-    required: true,
+const adminSchema = Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      match: /.+\@.+\..+/,
+    },
+    password: {
+      type: String,
+      min: 8,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+    },
+    companyName: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    pincode: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    state: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+    pancardNumber: {
+      type: String,
+    },
+    gstNumber: {
+      type: String,
+    },
+    license: {
+      type: String,
+    },
+    verification: {
+      type: String,
+      enum: ["Not Verified", "Verification Pending", "Verified"],
+      default: "Not Verified",
+      required: true,
+    },
+    domain: {
+      type: String,
+      enum: ["Bus", "Flight", "Hotel"],
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    unique: true,
-    match: /.+\@.+\..+/,
-  },
-  password: {
-    type: String,
-    min: 8,
-    required: true,
-  },
-  phoneNumber: {
-    type: Number,
-  },
-  companyName: {
-    type: String,
-  },
-  address: {
-    type: String,
-  },
-  pincode: {
-    type: String,
-  },
-  city: {
-    type: String,
-  },
-  state: {
-    type: String,
-  },
-  country: {
-    type: String,
-  },
-  pancardNumber: {
-    type: String,
-  },
-  gstNumber: {
-    type: String,
-  },
-  license: {
-    type: String,
-  },
-  verification: {
-    type: String,
-    enum: ["Not Verified", "Verification Pending", "Verified"],
-    default: "Not Verified",
-    required: true,
-  },
-  domain: {
-    type: String,
-    enum: ["Bus", "Flight", "Hotel"],
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const Admin = model("Admin", adminSchema);
