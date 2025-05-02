f89796a6c235a7472b2c54b985a10f7920590ff9
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-05-02 23:49:56 +0530

    Made neccesary bus routes

--- src/routes/bus.routes.js
+++ src/routes/bus.routes.js
@@ -1,6 +1,19 @@
 import { Router } from "express";
-import { createBus } from "../controllers/bus.controller";
+import {
+  createBus,
+  deleteBusData,
+  getActiveBusData,
+  getBusData,
+  getSingleBusData,
+  updateBusData,
+} from "../controllers/bus.controller";
+import { authenticate } from "../middleware/authenticate";
 
 export const busRouter = Router();
 
-busRouter.post("/create", createBus);
+busRouter.post("/create", authenticate("admin"), createBus);
+busRouter.get("/buses", authenticate("admin"), getBusData);
+busRouter.post("/active-buses", getActiveBusData);
+busRouter.post("/:id", getSingleBusData);
+busRouter.delete("delete/:id", authenticate("admin"), deleteBusData);
+busRouter.put("/update", authenticate("admin"), updateBusData);
