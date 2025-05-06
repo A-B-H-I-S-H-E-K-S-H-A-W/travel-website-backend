5006d1146d064fdb60a8f572ffe632f23e48c9f3
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-05-06 19:23:04 +0530

    Made a basic structure of hotel model

---
+++ src/models/hotel.models.js
@@ -0,0 +1,127 @@
+import { Schema, Types, model } from "mongoose";
+
+const hotelSchema = Schema(
+  {
+    owner: {
+      type: Types.ObjectId,
+      ref: "Admin",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      uppercase: true,
+    },
+    description: {
+      type: String,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ["Luxury", "Budget", "Boutique"],
+    },
+    highlight: {
+      type: [String],
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    landmark: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    pincode: {
+      type: String,
+      required: true,
+    },
+    checkInTime: {
+      type: Date,
+      required: true,
+    },
+    checkOutTime: {
+      type: Date,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      required: true,
+    },
+    images: {
+      type: [String],
+      default: [],
+      required: true,
+    },
+    totalBookings: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Hotel = model("Hotel", hotelSchema);
+
+const roomSchema = Schema(
+  {
+    owner: {
+      type: Types.ObjectId,
+      ref: "Hotel",
+      required: true,
+    },
+    bedType: {
+      type: String,
+      required: true,
+      enum: ["King Size", "Queen Size", "Twin"],
+    },
+    maxPerson: {
+      type: Number,
+      required: true,
+    },
+    facilities: {
+      type: [String],
+      required: true,
+    },
+    discount: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    payment: {
+      type: String,
+      required: true,
+      enum: ["Book now and pay online", "Book now and pay at hotel"],
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Room = model("Room", roomSchema);
