ebfd7bd2d384a5baba7eb7620e51cd5af2bbb3fd
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-05-07 00:00:11 +0530

    Added all functional routes

--- src/routes/hotel.routes.js
+++ src/routes/hotel.routes.js
@@ -1,6 +1,19 @@
 import { Router } from "express";
-import { createHotel } from "../controllers/hotel.controller.js";
+import {
+  createHotel,
+  deleteHotelData,
+  getActiveHotelData,
+  getHotelData,
+  getSingleHotelData,
+  updateHotelData,
+} from "../controllers/hotel.controller.js";
+import { authenticate } from "../middleware/authenticate.js";
 
 export const hotelRouter = Router();
 
-hotelRouter.get("/", createHotel);
+hotelRouter.post("/create", authenticate("admin"), createHotel);
+hotelRouter.get("/hotels", authenticate("admin"), getHotelData);
+hotelRouter.post("/active-hotels", getActiveHotelData);
+hotelRouter.get("/:id", getSingleHotelData);
+hotelRouter.delete("/delete/:id", authenticate("admin"), deleteHotelData);
+hotelRouter.put("/update", authenticate("admin"), updateHotelData);
