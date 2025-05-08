c4f1693e8f8b22d9c0252916f09ff17f288b508b
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-05-08 22:18:20 +0530

    Made functional route for booking

--- src/routes/booking.routes.js
+++ src/routes/booking.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import { Booking } from "../models/booking.models";
+import { authenticate } from "../middleware/authenticate";
 
 export const bookingRouter = Router();
 
-bookingRouter.post("/:id");
+bookingRouter.post("/book", authenticate("user"), Booking);
