3d458477112bc312f3dadea569727b49c0e76a76
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-05-09 14:55:27 +0530

    Bug fixed

--- src/utils/Duration.js
+++ src/utils/Duration.js
@@ -1,5 +1,5 @@
 export function duration(departureTime, arrivalTime) {
-  const diffMs = new Date(end) - new Date(start);
+  const diffMs = new Date(arrivalTime) - new Date(departureTime);
   const hours = Math.floor(diffMs / 3600000);
   const minutes = Math.floor((diffMs % 3600000) / 60000);
   return `${hours}h ${minutes}m`;
